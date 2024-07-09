42db537b902152d1aaed92382e151fa4730e6118
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2024-07-09 15:04:57 -0400

    first file creations

---
+++ first_test.py
@@ -0,0 +1,11 @@
+"""
+
+Serial Port: cu.usbserial-DP04WFZS
+Board ID: BoardIds.CYTON_DAISY_BOARD
+CYTON_DAISY_BOARD = 2
+
+API Docs = https://brainflow.readthedocs.io/en/stable/UserAPI.html#brainflow-board-shim
+
+"""
+
+print("test")
