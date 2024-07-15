d8d5591b75e1ef4b2582dc063b11dca7a817d810
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2024-07-15 10:06:39 -0400

    Working Example with command

--- first_example.py
+++ first_example.py
@@ -50,5 +50,6 @@ def main():
     print(data)
 
 
+
 if __name__ == "__main__":
     main()
--- first_test.py
+++ first_test.py
@@ -1,11 +1,78 @@
 """
 
-Serial Port: cu.usbserial-DP04WFZS
+Serial Port: /dev/cu.usbserial-DP04WFZS
 Board ID: BoardIds.CYTON_DAISY_BOARD
 CYTON_DAISY_BOARD = 2
+
+command: python3 first_test.py --board-id 2 --serial-port /dev/cu.usbserial-DP04WFZS
 
 API Docs = https://brainflow.readthedocs.io/en/stable/UserAPI.html#brainflow-board-shim
 
 """
 
-print("test")
+import argparse
+import time
+
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
+
+
+def main():
+
+    print("entered main")
+    BoardShim.enable_dev_board_logger()
+
+    parser = argparse.ArgumentParser()
+    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=True)
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    parser.add_argument('--master-board', type=int, help='master board id for streaming and playback boards',
+                        required=False, default=BoardIds.NO_BOARD)
+    args = parser.parse_args()
+
+    print("args passed and created")
+
+    params = BrainFlowInputParams()
+    params.ip_port = args.ip_port
+    params.serial_port = args.serial_port
+    params.mac_address = args.mac_address
+    params.other_info = args.other_info
+    params.serial_number = args.serial_number
+    params.ip_address = args.ip_address
+    params.ip_protocol = args.ip_protocol
+    params.timeout = args.timeout
+    params.file = args.file
+    params.master_board = args.master_board
+
+    board = BoardShim(args.board_id, params)
+    print("board object created")
+    board.prepare_session()
+    print("session prepared")
+    board.start_stream ()
+    time.sleep(10)
+
+    print("board object created time slept")
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    data = board.get_board_data()  # get all data and remove it from internal buffer
+
+    print("data created")
+    
+    board.stop_stream()
+    board.release_session()
+
+    print(data)
+
+
+
+if __name__ == "__main__":
+    main()
