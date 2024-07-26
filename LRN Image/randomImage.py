4b305f32bf45a531651e4b1379387b6c32d06cfd
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2024-07-25 20:43:43 -0400

    added a random left, right, and none generator for EEG data gathering

---
+++ LRN Image/randomImage.py
@@ -0,0 +1,44 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+import random
+import datetime
+
+
+# List of image file paths
+image_files = ["/Users/rithvikpraveenkumar/OpenBCI Project/LRN Image/Left Arrow Drawing.jpg", "/Users/rithvikpraveenkumar/OpenBCI Project/LRN Image/None drawing.jpg", "/Users/rithvikpraveenkumar/OpenBCI Project/LRN Image/Right Arrow Drawing.jpg"]
+timings = {}
+
+file = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN Image/dataLog.txt", "a")
+
+# Function to display a random image
+def display_random_image():
+    random_image = random.choice(image_files)
+    ct = datetime.datetime.now()
+    print(str(ct) + "  ------->  " + str(image_files.index(random_image)))
+    file.write(str(str(ct) + "  ------->  " + str(image_files.index(random_image)) + "\n"))
+    # 0 --> left
+    # 1 --> none
+    # 2 --> right
+    # timings[]
+    img = Image.open(random_image)
+    # img = img.resize((1200, 1200))  # Resize if needed
+    img_tk = ImageTk.PhotoImage(img)
+    label.config(image=img_tk)
+    label.image = img_tk
+    # Schedule next image display after a random interval
+    random_interval = random.randint(2, 4)  # Change interval range as needed
+    root.after(random_interval * 1000, display_random_image)
+
+# Create a Tkinter window
+root = tk.Tk()
+root.title("Random Image Display")
+
+# Create a label widget to display the images
+label = tk.Label(root)
+label.pack()
+
+# Start displaying images
+display_random_image()
+
+# Run the Tkinter event loop
+root.mainloop()
