ee790b8fbd6a3a616b213711be779f13991ae0ca
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2024-11-11 17:05:16 -0500

    added comments

--- LRN_Image/matching.py
+++ LRN_Image/matching.py
@@ -1,30 +1,43 @@
-eeg_file = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/EEGData.txt", "r")
-lr_file = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/LRNImageData.txt", "r")
-labeledData = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/LabeledData.txt", "w")
+"""
 
+This program is meant to match and merge the EEG signals and the corresponding direction (L, R, N).
+It generates an output file with the matched EEG signals, the respective direction, and the timestamps used to match them.
+
+"""
+
+
+# open/access necessary files
+# --------------------
+eeg_file = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/EEGData.txt", "r") # File with EEG signals and timestamps
+lr_file = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/LRNImageData.txt", "r") # File with Left, Right, or None and timestamps
+labeledData = open("/Users/rithvikpraveenkumar/OpenBCI Project/LRN_Image/LabeledData.txt", "w") # Final output file (starts off blank and gets written to in the end)
+
+# Read the files
 lrlines = lr_file.readlines()
 eeglines = eeg_file.readlines()
+# --------------------
 
+# Loops through all the lines of the lr_file
 for index in range(1, len(lrlines)):
     
+    # Establishes timestamp range to look within
     lowerTimestamp = float(lrlines[index - 1].split("  ------->  ")[1])
     upperTimestamp = float(lrlines[index].split("  ------->  ")[1])
 
-
+    # Establishes which action to match to within the timestamp range
     if(int(lrlines[index - 1].split("  ------->  ")[2]) == 0):
         currentAction = "Left"
     elif(int(lrlines[index - 1].split("  ------->  ")[2]) == 1):
         currentAction = "None"
     else:
         currentAction = "Right"
-        
+
+    # Loops through every relevant line of the eeg_file    
     for eegline in range(5, len(eeglines)):
         eeglineArray = eeglines[eegline].split(", ")
         eegTimestamp = float(eeglineArray[-3])
-        if eegTimestamp >= lowerTimestamp:
-            if eegTimestamp < upperTimestamp:
-                print(lowerTimestamp, eegTimestamp, upperTimestamp)
+
+        # Checks whether currect timestamp is within the desired timestamp range
+        if (eegTimestamp >= lowerTimestamp) and (eegTimestamp < upperTimestamp):
+                # Writes the appropriate eeg, timestamp, and direction data to the output file if timestamp is within the range
                 labeledData.write(str((currentAction, str(lowerTimestamp), str(eegTimestamp), str(upperTimestamp), str(eeglineArray[1:16]))) + "\n")
-
-
-
